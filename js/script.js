cabffe8513f718c5aa1dd0460889c7da205543e7
mariam-elmadawy <91071088+mariam-elmadawy@users.noreply.github.com>
2023-02-22 21:35:21 +0200

    update all files

--- js/script.js
+++ js/script.js
@@ -1,21 +1,6 @@
-const container = document.querySelector('.container');
-const subBtn = document.querySelector('.btn');
+const moreBtn = document.querySelector('.more');
+const view = document.querySelector('.gallery .container')
+moreBtn.addEventListener('click', () => {
+    view.style.display = 'flex'
 
-const popup = document.querySelector('.popup');
-const close = document.querySelector('.x')
-const join = document.querySelector('.join')
-
-subBtn.addEventListener('click', () => {
-    container.classList.add('active')
-    popup.classList.remove('active')
-})
-
-close.addEventListener('click', () => {
-    container.classList.remove('active')
-    popup.classList.add('active')
-})
-
-join.addEventListener('click', () => {
-    container.classList.remove('active')
-    popup.classList.add('active')
 })
